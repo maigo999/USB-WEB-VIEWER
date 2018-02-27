74ebb13a03a39a313898bcde710ba9643db10f65
maigo999 <roborobotj3@gmail.com>
2018-02-27 18:19:06 +0900

    fix output

--- js/index.js
+++
@@ -1,4 +0,0 @@
-function login() {
-	$('form').fadeOut(2000);
-	$('.wrapper').addClass('form-success');
-  }
--- js/usb.js
+++ js/usb.js
@@ -3,10 +3,11 @@
 const auth = () => {
     navigator.usb.requestDevice({ 'filters': []
     }).then(device => {
-
-        var fixvendorId = ( '0000' + device.vendorId.toString(16).toUpperCase() ).slice(-4);
-        var fixproductId = ( '0000' + device.productId.toString(16).toUpperCase() ).slice(-4);
-        alert('0x' + fixvendorId + '\n' + '0x' + fixproductId + '\n' + device.serialNumber)
+        var fixvendorId = ('0000' + device.vendorId.toString(16).toUpperCase() ).slice(-4);
+        var fixproductId = ('0x' + '0000' + device.productId.toString(16).toUpperCase() ).slice(-4);
+        console.log('0x' + fixvendorId);
+        console.log('0x' + fixproductId);
+        console.log(device.serialNumber);
     }).catch(error => {
     console.log(error);
     });
