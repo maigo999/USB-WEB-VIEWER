c130fd3c27c56c025a8b90280e7a66ca44aa0a75
maigo999 <roborobotj3@gmail.com>
2018-02-27 19:37:38 +0900

    fix data

--- js/usb.js
+++ js/usb.js
@@ -4,11 +4,22 @@ const auth = () => {
     navigator.usb.requestDevice({ 'filters': []
     }).then(device => {
         var fixvendorId = ('0000' + device.vendorId.toString(16).toUpperCase() ).slice(-4);
-        var fixproductId = ('0x' + '0000' + device.productId.toString(16).toUpperCase() ).slice(-4);
+        var fixproductId = ('0000' + device.productId.toString(16).toUpperCase() ).slice(-4);
+        var target1 = document.getElementById("Vendorid");
+        var target2 = document.getElementById("Productid");
+        var target3 = document.getElementById("Serialnumber");
         console.log('0x' + fixvendorId);
         console.log('0x' + fixproductId);
         console.log(device.serialNumber);
-    }).catch(error => {
-    console.log(error);
-    });
-}
+        target1.innerHTML = '0x' + fixvendorId;
+        target2.innerHTML = '0x' + fixproductId;
+        
+        if (!device.serialNumber){
+            target3.innerHTML = "シリアルナンバーがありません";
+        }else{
+            target3.innerHTML = device.serialNumber;
+        }
+        }).catch(error => {
+            console.log(error);
+        });
+    }
