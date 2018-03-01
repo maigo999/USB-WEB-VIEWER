71e0493537992b3d16b57b192f3c3b100f9a8459
maigo999 <roborobotj3@gmail.com>
2018-03-01 09:49:33 +0900

    add docs js

---
+++ docs/js/usb.js
@@ -0,0 +1,25 @@
+'use strict';
+ 
+const auth = () => {
+    navigator.usb.requestDevice({ 'filters': []
+    }).then(device => {
+        var fixvendorId = ('0000' + device.vendorId.toString(16).toUpperCase() ).slice(-4);
+        var fixproductId = ('0000' + device.productId.toString(16).toUpperCase() ).slice(-4);
+        var target1 = document.getElementById("Vendorid");
+        var target2 = document.getElementById("Productid");
+        var target3 = document.getElementById("Serialnumber");
+        console.log('0x' + fixvendorId);
+        console.log('0x' + fixproductId);
+        console.log(device.serialNumber);
+        target1.innerHTML = '0x' + fixvendorId;
+        target2.innerHTML = '0x' + fixproductId;
+
+        if (!device.serialNumber){
+            target3.innerHTML = "SerialNumberがありません";
+        }else{
+            target3.innerHTML = device.serialNumber;
+        }
+        }).catch(error => {
+            console.log(error);
+        });
+    }
